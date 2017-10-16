8a56d6ddd4be8f1d97c477671d121699d9f49bfc
DT41guodong <32757150+DT41guodong@users.noreply.github.com>
2017-10-16 22:15:51 +0800

    Update Student.java

--- src/main/java/cn/java/model/Student.java
+++ src/main/java/cn/java/model/Student.java
@@ -2,6 +2,6 @@ package cn.java.model;
 
 public class Student {
     public void show() {
-        System.out.println("1321");
+        System.out.println("1321");System.out.println("1321");
     }
 }
